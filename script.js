0c4138b506e8a5d0191b4d6fe20cdd90675610a0
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-10 13:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let level=document.getElementById("level");
+let i=0;
+while(level.parentNode){
+	 level=level.parentNode;
+	i++;
+}
+
+alert("The level of the element is: "+i);
